adad2289cad1e11401f50257458d73c3ddcfbd33
Yannis Panagis <yannis.panagis1998@gmail.com>
2020-04-30 17:48:56 +0100

    dark mode toggle updated to emojis

--- src/components/darkModeToggle.js
+++ src/components/darkModeToggle.js
@@ -11,14 +11,14 @@ export default props => {
             aria-label="light mode"
             onClick={darkMode.disable}
             className="sunIcon"
-          />
+          ><span>️☀️</span></button>
         ) : (
           <button
             type="button"
             aria-label="dark mode"
             onClick={darkMode.enable}
             className="moonIcon"
-          />
+          ><span>️🌙</span></button>
         )}
       </>
     )
