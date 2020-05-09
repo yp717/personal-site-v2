6a54d84b52f009af0ed00779e429dcb40dfa46a7
Yannis Panagis <yannis.panagis1998@gmail.com>
2020-05-09 21:18:00 +0100

    updated typography

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = () => (
               <p className="is-greeting-blue-text greeting-text">
                 Hi <span role="img" aria-label="wave">👋</span>, my name is
               </p>         
-              <h1 className="is-background-blue-text">Yannis Panagis</h1>
+              <h1 className="is-background-blue-text margin-t-b-0" style={{fontSize: '2.8em', fontFamily: 'Lato-Bold'}}>Yannis Panagis</h1>
               <h2 className="hero-tagline-text is-background-blue-text text-align-center">Engineer. Designer. Developer. Student.</h2>
               <SocialLinks/>
           </div>
