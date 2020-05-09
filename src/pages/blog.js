0c6e66862fc343241227ea1188b56d52029dc74d
Yannis Panagis <yannis.panagis1998@gmail.com>
2020-05-09 19:28:24 +0100

    added same style as projects page to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -10,42 +10,42 @@ import BuyMeCoffeeCard from '../components/cards/buyMeCoffeeCard';
 const BlogPage = ({ data }) => {
     const posts = data.allMarkdownRemark.edges;
     return (
-    <Layout>
-        <SEO title="Blog" keywords={[`gatsby`, `application`, `react`]} />  
-        <div className="container margin-b-5">
-            <div className="container__row">
-                <div className="container__col-sm-12">
-                    <h1 className="is-background-blue-text margin-l-r-5">Blog</h1>
-                </div>
-            </div>
-            {posts.map(post => (
-                    <div key={post.node.id} className={`container__row content-card is-${post.node.frontmatter.color}-border padding-0`} style={{width: '90%'}}>
-                            <div className="container__col-sm-12 container__col-lg-4">
-                                <Img
-                                    fluid={post.node.frontmatter.thumbnail.childImageSharp.fluid}
-                                    style={{ maxHeight: 250 }}
-                                />
-                            </div>
-                            <div className="container__col-sm-12 container__col-lg-6" style={{ padding: '2%'}}>
-                                <h2 style={{fontSize: '25px'}}className="is-background-blue-text">{post.node.frontmatter.title}</h2>
-                                <p className="is-background-blue-text">{post.node.frontmatter.date}</p>
-                                {post.node.frontmatter.tags ? (
-                                    <div style={{marginTop: '5px', marginBottom: '5px'}}>
-                                        {post.node.frontmatter.tags.map(tag => (
-                                            <Link className="post-tags margin-r-1" to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                                        ))}
-                                    </div>    
-                                ) : null}
-                                <div className="is-background-blue-text">{post.node.excerpt}</div>
-                                <Link to={post.node.fields.slug}>
-                                    Read More about it
-                                </Link>
-                            </div>
+        <Layout>
+            <SEO title="Blog" keywords={[`gatsby`, `blog`, `instructional`, `tutorials`, `software engineering`, `coding`]}/>
+            <div className="container margin-b-5">
+                <div className="container__row">
+                    <div className="container__col-sm-12">
+                        <h1 className="is-background-blue-text margin-l-r-5">Blog</h1>
                     </div>
-            ))}
-        </div>
-        <BuyMeCoffeeCard/>
-    </Layout>
+                </div>    
+                    {posts.map(post => (
+                        <Link to={post.node.fields.slug} className="margin-0 padding-0">
+                            <div key={post.node.id} className={`container__row content-card card-link padding-0`} style={{width: '90%'}}>
+                                <div className="container__col-sm-12 container__col-lg-5">
+                                    <Img
+                                        fluid={post.node.frontmatter.thumbnail.childImageSharp.fluid}
+                                        style={{ maxHeight: 250 }}
+                                    />
+                                </div> 
+                                <div className="container__col-sm-12 container__col-lg-6" style={{ padding: '2%'}}>
+                                    <h2 style={{fontSize: '25px'}}className="is-background-blue-text">{post.node.frontmatter.title}</h2>
+                                        <p className="is-background-blue-text">{post.node.frontmatter.date}</p>
+                                        {/* {post.node.frontmatter.tags ? (
+                                            <div>
+                                                {post.node.frontmatter.tags.map(tag => (
+                                                    <Link className="post-tags margin-r-1" to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                                                ))}
+                                            </div>    
+                                        ) : null} */}
+                                        {/* There needs to be a sass class here to correctly set the body text formatting */}
+                                        <p className="is-background-blue-text" >{post.node.excerpt}</p>
+                                </div> 
+                            </div>   
+                        </Link>     
+                    ))}
+            </div>
+            {/* <BuyMeCoffeeCard/> */}
+        </Layout>
     );
 };
 
