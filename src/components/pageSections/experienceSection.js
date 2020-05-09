7a5944b48aa747abd914ed4a361a4e4716b7e82e
Yannis Panagis <yannis.panagis1998@gmail.com>
2020-05-09 12:06:59 +0100

    added animation on scroll functionality

--- src/components/pageSections/experienceSection.js
+++ src/components/pageSections/experienceSection.js
@@ -8,36 +8,64 @@ const ExperienceSection = () => {
         <h2 className="is-background-blue-text margin-left-right-5">Experience<span role="img" aria-label="nerd">🤓</span></h2>
             <div className="container__row">
                 {/* experience: Private Tutoring*/}
-                <ExperienceCard 
-                    title="Private Tutoring"
-                    subheading="Self-organised"
-                    date ="June 2019 - Present"
-                    description="Tutoring A-Level, GCSE and University students in Python, Computer Science concepts and Mathematics."
-                />
-
+                <div
+                    data-sal="flip-up"
+                    data-sal-delay="300"
+                    data-sal-easing="ease"
+                    data-sal-duration="300"
+                >
+                    <ExperienceCard 
+                        title="Private Tutoring"
+                        subheading="Self-organised"
+                        date ="June 2019 - Present"
+                        description="Tutoring A-Level, GCSE and University students in Python, Computer Science concepts and Mathematics."
+                    />
+                </div>
+                
                 {/* experience: American Express*/}
-                <ExperienceCard 
-                    title="Technology Summer Intern"
-                    subheading="American Express"
-                    date ="June 2019 - Aug 2019"
-                    description="Designed, developed and put in production a fully functional research archive solution with ReactJS, GatsbyJS, ExpressJS, and python for the machine learning based backend. Participated in design sprints to determine product requirements, develop wireframes, prototype UX/UI designs"
-                />
+                <div
+                    data-sal="flip-left"
+                    data-sal-delay="300"
+                    data-sal-easing="ease"
+                    data-sal-duration="300"
+                >
+                    <ExperienceCard 
+                        title="Technology Summer Intern"
+                        subheading="American Express"
+                        date ="June 2019 - Aug 2019"
+                        description="Designed, developed and put in production a fully functional research archive solution with ReactJS, GatsbyJS, ExpressJS, and python for the machine learning based backend. Participated in design sprints to determine product requirements, develop wireframes, prototype UX/UI designs"
+                    />
+                </div>
 
                 {/* experience: Firetech Camp Tutor */}
-                <ExperienceCard 
-                    title="Firetech Camp Tutor"
-                    subheading="Firetech"
-                    date ="June 2019 - ongoing"
-                    description="Teaching courses to young students to explore coding, robotics, app design, making electronics and more during term-time"
-                />
+                <div
+                    data-sal="flip-down"
+                    data-sal-delay="300"
+                    data-sal-easing="ease"
+                    data-sal-duration="300"
+                >
+                    <ExperienceCard 
+                        title="Firetech Camp Tutor"
+                        subheading="Firetech"
+                        date ="June 2019 - ongoing"
+                        description="Teaching courses to young students to explore coding, robotics, app design, making electronics and more during term-time"
+                    />
+                </div>
 
-                {/* experience: Corporate Investment Intern*/}
-                <ExperienceCard 
-                    title="Corporate Investment Intern"
-                    subheading="Investcorp B.S.C."
-                    date ="June 2016 - Aug 2016"
-                    description="Analysed corporate financial data to build models to examine companies and markets to determine investment viability for 10 potential acquisitions"
-                />
+                <div
+                    data-sal="flip-right"
+                    data-sal-delay="300"
+                    data-sal-duration="300"
+                    data-sal-easing="ease"
+                >
+                    {/* experience: Corporate Investment Intern*/}
+                    <ExperienceCard 
+                        title="Corporate Investment Intern"
+                        subheading="Investcorp B.S.C."
+                        date ="June 2016 - Aug 2016"
+                        description="Analysed corporate financial data to build models to examine companies and markets to determine investment viability for 10 potential acquisitions"
+                    />
+                </div>
             </div>
         </div>
         </>
