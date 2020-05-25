3d357f65eda2631a3ff0c5c65fa4298dcfe3abd6
Yannis Panagis <yannis.panagis1998@gmail.com>
2020-05-25 16:55:06 +0100

    gatsby plugin netlify header configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,6 +128,18 @@ module.exports = {
     // Read more @ https://www.gatsbyjs.org/packages/gatsby-plugin-smoothscroll/
     // can be used for scroll to animations
     `gatsby-plugin-smoothscroll`,
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
