6b4e81e5bbfb28c9a9fd5dec55032c30e784a9d3
Yannis Panagis <yannis.panagis1998@gmail.com>
2020-05-25 12:29:54 +0100

    updated page preview text for link sharing imprvmt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Yannis Panagis`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Engineer. Designer. Developer. Student.`,
     author: `@gatsbyjs`,
     menuLinks:[
       {
