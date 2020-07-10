69a51f7f91f2d15a2b9bf7e218817441cbc3216e
Yannis Panagis <yannis.panagis1998@gmail.com>
2020-07-10 17:16:53 +0100

    buy me a coffee section is now responsive

--- src/components/cards/buyMeCoffeeCard.js
+++ src/components/cards/buyMeCoffeeCard.js
@@ -4,13 +4,15 @@ import LegoYannisFront from "../../images/LEGOs/lego-yannis-front.svg";
 
 const BuyMeCoffeeCard = () => (
     <>
-        <div className="container row content-card margin-5-lr margin-3-t is-white-bg">
-            <div className="col-xs-3 text-align-center">
+        <div className="container row flex content-card margin-5-lr margin-3-t is-white-bg" style={{ justifyContent: "center" }}>
+            <div className="col-xs-12 col-sm-3 flex align-vertical align-horizontal">
                 <img src={LegoYannisFront} alt="lego-icon-front"/>
             </div>
-            <div className="col-xs-9">
-                <p className="is-background-blue-text">I don't have any ads or sponsors. If you enjoyed the content on this site and found it helpful, interesting or fun, please consider supporting this site by buying me a coffee.</p>
-                <div className="margin-2 margin-l-0">
+            <div className="col-xs-12 col-sm-9">    
+                <p className="is-background-blue-text margin-2 margin-b-0">
+                    I don't have any ads or sponsors. If you enjoyed the content on this site and found it helpful, interesting or fun, please consider supporting this site by buying me a coffee.
+                </p>
+                <div className="margin-2 margin-b-0">
                     <BuyMeCoffeeButton/>
                 </div>
             </div>
