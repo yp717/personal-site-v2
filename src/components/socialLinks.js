bedf5a39b868a11e257be4c2b382671cd3794adf
Yannis Panagis <yannis.panagis1998@gmail.com>
2020-07-10 16:50:31 +0100

    consistent icons across light and dark mode

--- src/components/socialLinks.js
+++ src/components/socialLinks.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import useDarkMode from "use-dark-mode";
 
 import SpotifyIcon from '../images/icons/social/spotify.svg';
-import SpotifyIconWhite from '../images/icons/social/spotify-white.svg';
 import GithubIcon from '../images/icons/social/github.svg';
-import GithubIconWhite from '../images/icons/social/github-white.svg';
 import LinkedInIcon from '../images/icons/social/linked-in.svg';
-import LinkedInIconWhite from '../images/icons/social/linked-in-white.svg';
 import MailIcon from '../images/icons/social/mail.svg';
-import MailIconWhite from '../images/icons/social/mail-white.svg';
 
 const githubLink = `https://github.com/yp717`;
 const linkedinLink = `https://www.linkedin.com/in/yannis-panagis/`;
@@ -16,89 +11,44 @@ const spotifyLink = `https://open.spotify.com/artist/4tCn4Wkn4cf0CWLUFvQO58?si=X
 const mailLink = `mailto:yannis.panagis1998@gmail.com`;
 
 const SocialLinks = () => {
-    const darkMode = useDarkMode(false) //defaults to false 
     return (
         <>
             <div className="text-align-center">
-                <a href={githubLink} target="blank">
-                    { 
-                        darkMode.value ?
-                        (
-                            <img 
-                                className="socialIcon" 
-                                aria-label="github icon" 
-                                alt="github icon"
-                                src={GithubIconWhite}/>
-                        ) :
-                        (
-                            <img 
-                                className="socialIcon" 
-                                aria-label="github" 
-                                alt="github icon"
-                                src={GithubIcon}/>
-                        )
-                    }
+                <a href={githubLink} target="blank">    
+                    <img 
+                        className="socialIcon" 
+                        aria-label="github" 
+                        alt="github icon"
+                        src={GithubIcon}
+                    />
                 </a>
                 <a href={linkedinLink} target="blank">
-                    {
-                        darkMode.value ?
-                        (
-                            <img 
-                                className="socialIcon" 
-                                aria-label="linkedin" 
-                                alt="linkedin icon"
-                                src={LinkedInIconWhite}/>
-                        ) :
-                        (
-                            <img 
-                                className="socialIcon" 
-                                aria-label="linkedin" 
-                                alt="linkedin icon"
-                                src={LinkedInIcon}/>
-                        )
-                    } 
+                    <img 
+                        className="socialIcon" 
+                        aria-label="linkedin" 
+                        alt="linkedin icon"
+                        src={LinkedInIcon}
+                    />
                 </a>
                 <a href={spotifyLink} target="blank">
-                    {
-                        darkMode.value ? 
-                        (
-                            <img 
-                                className="socialIcon" 
-                                aria-label="spotify" 
-                                alt="spotify icon"
-                                src={SpotifyIconWhite}/>
-                        ) :
-                        (
-                            <img 
-                                className="socialIcon" 
-                                aria-label="spotify" 
-                                alt="spotify icon"
-                                src={SpotifyIcon}/>
-                        )
-                    }
+                    <img 
+                        className="socialIcon" 
+                        aria-label="spotify" 
+                        alt="spotify icon"
+                        src={SpotifyIcon}
+                    />
                 </a>
                 <a href={mailLink} target="blank">
-                    {
-                        darkMode.value ?
-                        (
-                            <img 
-                                className="socialIcon" 
-                                aria-label="mail" 
-                                alt="mail icon"
-                                src={MailIconWhite}/>
-                        ) :
-                        (
-                            <img 
-                                className="socialIcon" 
-                                aria-label="mail" 
-                                alt="mail icon"
-                                src={MailIcon}/>
-                        )
-                    }
+                    <img 
+                        className="socialIcon" 
+                        aria-label="mail" 
+                        alt="mail icon"
+                        src={MailIcon}
+                    />
                 </a>
-            </div>    
+            </div>
         </>
     );
 }
 
-export default SocialLinks;    
+export default SocialLinks;
