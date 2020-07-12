dafc3cdfa5156cb432aff470629581e653fa050b
Yannis Panagis <yannis.panagis1998@gmail.com>
2020-07-12 12:28:21 +0100

    tag Template style matched

--- src/templates/tagTemplate.js
+++ src/templates/tagTemplate.js
@@ -4,6 +4,7 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 // import Img from 'gatsby-image';
 import { kebabCase } from 'lodash';
+import PostPreviewCard from '../components/cards/postPreviewCard';
 
 const Tags = ({ pageContext, data }) => {
     const { tag } = pageContext;
@@ -23,31 +24,25 @@ const Tags = ({ pageContext, data }) => {
                 <h1 className="is-background-blue-text">{tagHeader}</h1>
               </div>
               <div className="col-xs-12 pad-5-lr margin-5-b">          
-                {edges.map(({ node }) => {
-                  const { title, date, tags } = node.frontmatter;
-                  const { slug } = node.fields;
-                  return (
-                    <Link to={`/${slug}`} className="margin-0 padding-0">
-                      <div key={node.id} className="content-card is-white-bg margin-5-b is-deep-sky-blue-border">
-                        <Link to={`/${slug}`} className="margin-0">
-                          <h2 className="post-tags">{title}</h2>
-                        </Link>
-                        <p className="is-background-blue-text">{date}</p>
-                        {tags ? (
-                            <div>
-                                {tags.map(tag => (
-                                    <Link className="active underline" to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                                ))}
-                            </div>    
-                        ) : null}
-                      </div>
-                    </Link>
-                  );
-                })}
+                {
+                  edges.map(({ node }) => (
+                    <PostPreviewCard 
+                      id={node.id}
+                      slug={node.frontmatter.slug}
+                      title={node.frontmatter.title}
+                      thumbnail={node.frontmatter.thumbnail.childImageSharp.fluid}
+                      date={node.frontmatter.date}
+                      tags={node.frontmatter.tags}
+                      desc={node.frontmatter.description}
+                    />
+                  )
+                )}
               </div>        
-              <div className="col-xs-12 pad-5-r">
+              <div className="pad-5-lr">
                 <Link to="/tags">
-                  <h2 className="post-tags" style={{paddingLeft: '12px'}}>All tags</h2>
+                  <h2 className="is-background-blue-text active underline">
+                    All tags
+                  </h2>
                 </Link>    
               </div>
             </div>
